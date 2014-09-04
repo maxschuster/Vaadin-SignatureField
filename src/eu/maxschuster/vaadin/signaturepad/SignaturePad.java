f5b4a6c0735006f08e759087e4acfd00295d29f7
Max Schuster <dev@maxschuster.eu>
2014-09-04 22:08:53 +0200

    Initial commit

---
+++ src/eu/maxschuster/vaadin/signaturepad/SignaturePad.java
@@ -0,0 +1,15 @@
+package eu.maxschuster.vaadin.signaturepad;
+
+import com.vaadin.annotations.JavaScript;
+import com.vaadin.annotations.StyleSheet;
+import com.vaadin.ui.AbstractJavaScriptComponent;
+
+@JavaScript({
+	"js/vaadin.SignaturePad.js"
+})
+@StyleSheet({
+	"css/vaadin.SignaturePad.css"
+})
+public class SignaturePad extends AbstractJavaScriptComponent {
+
+}
